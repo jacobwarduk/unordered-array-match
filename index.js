ab7f0d6fba3d93ae73e5467e0967c29d0b5703a6
Jacob Ward <jacob@candyboxmedia.com>
2017-06-09 10:54:21 +0100

    replace jasmine-node with jasmine and add xo linting (including fixes)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function unorderedArrayMatch(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
-    return false;
-  }
+	if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+		return false;
+	}
 
-  return arr1.every(function(item) {
-    return arr2.indexOf(item) !== -1;
-  });
+	return arr1.every(item => {
+		return arr2.indexOf(item) !== -1;
+	});
 }
 
 module.exports = unorderedArrayMatch;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,35 +1,36 @@
+/* global describe it expect */
 'use strict';
 
-var unorderedArrayMatch = require('../index');
+const unorderedArrayMatch = require('../index');
 
-describe('unorderedArrayMatch', function() {
-  it('should return true when both arrays contain the same items in any order', function() {
-    expect(unorderedArrayMatch(['a', 23, 'pineapple'], ['pineapple', 'a', 23])).toBe(true);
-    expect(unorderedArrayMatch(['a', 'b', 'c', 1, 2, 3], [3, 'c', 2, 'b', 1, 'a'])).toBe(true);
-  });
+describe('unorderedArrayMatch', () => {
+	it('should return true when both arrays contain the same items in any order', () => {
+		expect(unorderedArrayMatch(['a', 23, 'pineapple'], ['pineapple', 'a', 23])).toBe(true);
+		expect(unorderedArrayMatch(['a', 'b', 'c', 1, 2, 3], [3, 'c', 2, 'b', 1, 'a'])).toBe(true);
+	});
 
-  it('should return false when arrays do not contain the same items', function() {
-    expect(unorderedArrayMatch(['a', 23], [23, 'coconut'])).toBe(false);
-  });
+	it('should return false when arrays do not contain the same items', () => {
+		expect(unorderedArrayMatch(['a', 23], [23, 'coconut'])).toBe(false);
+	});
 
-  it('should return false when arrays are of different lengths', function() {
-    expect(unorderedArrayMatch(['a', 23], [42])).toBe(false);
-  });
+	it('should return false when arrays are of different lengths', () => {
+		expect(unorderedArrayMatch(['a', 23], [42])).toBe(false);
+	});
 
-  it('should return false when only one array is provided as an argument', function() {
-    expect(unorderedArrayMatch(['a', 23])).toBe(false);
-  });
+	it('should return false when only one array is provided as an argument', () => {
+		expect(unorderedArrayMatch(['a', 23])).toBe(false);
+	});
 
-  it('should return false when no arguments are provided', function() {
-    expect(unorderedArrayMatch()).toBe(false);
-  });
+	it('should return false when no arguments are provided', () => {
+		expect(unorderedArrayMatch()).toBe(false);
+	});
 
-  it('should return false when provided arguments are not an array', function() {
-    expect(unorderedArrayMatch(['a', 23], {})).toBe(false);
-    expect(unorderedArrayMatch(['a', 23], function() {})).toBe(false);
-    expect(unorderedArrayMatch(['a', 23], null)).toBe(false);
-    expect(unorderedArrayMatch('a string', ['a', 23])).toBe(false);
-    expect(unorderedArrayMatch(23, ['a', 23])).toBe(false);
-    expect(unorderedArrayMatch(null, null)).toBe(false);
-  })
+	it('should return false when provided arguments are not an array', () => {
+		expect(unorderedArrayMatch(['a', 23], {})).toBe(false);
+		expect(unorderedArrayMatch(['a', 23], () => {})).toBe(false);
+		expect(unorderedArrayMatch(['a', 23], null)).toBe(false);
+		expect(unorderedArrayMatch('a string', ['a', 23])).toBe(false);
+		expect(unorderedArrayMatch(23, ['a', 23])).toBe(false);
+		expect(unorderedArrayMatch(null, null)).toBe(false);
+	});
 });
